782076c27eb0db10c7f57122f62150fa08cae6cb
ichte <xt2810@gmail.com>
2017-03-19 17:58:28 +0700

    Add files via upload

---
+++ ichte/utilities.java
@@ -0,0 +1,25 @@
+package ichte;
+
+import java.io.IOException;
+
+/**
+ * Created by XT on 3/19/2017.
+ */
+
+
+public class utilities {
+    public static boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        }
+        catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+
+        return false;
+    }
+
+}
+
